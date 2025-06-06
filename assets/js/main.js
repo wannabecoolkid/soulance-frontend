1fcaceb43669aebb3b9ba2fe11cbe3e177f3cc1d
wannabecoolkid <166440416+wannabecoolkid@users.noreply.github.com>
2025-06-06 15:56:26 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+/*=============== SHOW HIDE PASSWORD LOGIN ===============*/
+const passwordAccess = (loginPass, loginEye) =>{
+   const input = document.getElementById(loginPass),
+         iconEye = document.getElementById(loginEye)
+
+    iconEye.addEventListener('click', ()=>{
+        //Change password to tect
+        input.type ==='password' ? input.type = 'text'
+                                 : input.type = 'password'
+         
+        // Icon change
+        iconEye.classList.toggle('ri-eye-fill') 
+        iconEye.classList.toggle('ri-eye-off-fill')
+
+    })
+}
+passwordAccess('password','loginPassword')
+/*=============== SHOW HIDE PASSWORD CREATE ACCOUNT ===============*/
+const passwordRegister = (loginPass, loginEye) =>{
+   const input = document.getElementById(loginPass),
+         iconEye = document.getElementById(loginEye)
+
+   iconEye.addEventListener('click', () =>{
+      // Change password to text
+      input.type === 'password' ? input.type = 'text'
+						              : input.type = 'password'
+
+      // Icon change
+      iconEye.classList.toggle('ri-eye-fill')
+      iconEye.classList.toggle('ri-eye-off-fill')
+   })
+}
+passwordRegister('passwordCreate','loginPasswordCreate')
+
+/*=============== SHOW HIDE LOGIN & CREATE ACCOUNT ===============*/
+const loginAccessRegister = document.getElementById('loginAccessRegister'),
+       buttonRegister = document.getElementById('loginButtonRegister'),
+       buttonAccess = document.getElementById('loginButtonAccess')
+
+buttonRegister.addEventListener('click',()=>{
+    loginAccessRegister.classList.add('active')
+})
+buttonAccess.addEventListener('click',()=>{
+    loginAccessRegister.classList.remove('active')
+})
